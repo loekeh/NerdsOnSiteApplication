8cf4fd9a8a08d2e896d4fd400c08473a526d9b15
Longinus <longinus.ekeh@wcbizc.com>
2017-01-12 19:20:50 -0700

    Add project files.

---
+++ NerdsOnSiteApplication/Models/CarMetada.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NerdsOnSiteApplication.Models
+{
+    [MetadataType(typeof(Car.Metadata))]
+    
+    public partial class Car
+    {       
+            sealed class Metadata
+            {
+
+                public int CarId { get; set; }
+                [Required]
+                [Display(Name = "Car Manufacturer")]
+                public string CarManufacturer { get; set; }
+
+                [Required]
+                [Display(Name = "Car Make")]
+                public string CarMake { get; set; }
+
+                [Required]
+                [Display(Name = "Year Of Car")]
+                public System.DateTime CarYear { get; set; }
+
+               [Required]
+               [Display(Name = "Color Of Car")]
+                public string Color { get; set; }
+               [Required]
+               [Display(Name = "Passenger Seating")]
+                public int PassengerSeating { get; set; }
+            }
+        }
+    }
+
